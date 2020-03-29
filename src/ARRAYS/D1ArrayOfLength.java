4dc7b1edb851920d3a7f82b719521c6e873f140f
inci-k <ikaratay@na.edu>
2020-03-29 01:22:12 -0500

    March 29

--- src/ARRAYS/D1ArrayOfLength.java
+++ src/ARRAYS/D1ArrayOfLength.java
@@ -2,7 +2,7 @@ package ARRAYS;
 
 public class D1ArrayOfLength {
     public static void main(String[] args) {
-        int[] a = new int[5];
+        //int[] a = new int[5];
         int[] b = {12,13,14,15,16,17};
         System.out.println("The length of the array = "+b.length);
     }
