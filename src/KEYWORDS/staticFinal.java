58d7c51ad3fd396452eaf6deebeb7be3ba8a0300
inci-k <ikaratay@na.edu>
2020-04-24 00:33:48 -0500

    April 24

---
+++ src/KEYWORDS/staticFinal.java
@@ -0,0 +1,7 @@
+package KEYWORDS;
+
+public class staticFinal {
+    public static final String username = "test";
+    public static final String password = "1234";
+
+}
---
+++ src/KEYWORDS/staticFinalTEST.java
@@ -0,0 +1,12 @@
+package KEYWORDS;
+
+public class staticFinalTEST {
+    public static void main(String[] args) {
+        System.out.println("User name :"+staticFinal.username);
+        System.out.println("Password  :"+staticFinal.password);
+
+
+        // "public static final double"  write in the java system
+        System.out.println("PI number ="+Math.PI);
+    }
+}
